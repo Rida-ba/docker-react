7b1e3d8264c55192405f72f4bfc477c3599eafc6
Rida BAOUDENE <rbaoudene@icloud.com>
2020-04-02 18:12:33 +0200

    changed app test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          We can move to the next step.
+          React App on AWS with CI/CD.
         </p>
         <a
           className="App-link"
