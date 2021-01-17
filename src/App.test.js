da291c9111711db95d885f86c45761a5a1fd4ea0
Rida BAOUDENE <rbaoudene@icloud.com>
2021-01-17 17:08:04 +0100

    Add test renders text Vaudoise

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders visit vaudoise assurances', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/visit vaudoise assurances/i);
   expect(linkElement).toBeInTheDocument();
 });
